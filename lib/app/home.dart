b88b77f9455fb4b66a66e5902a231b09a96618f8
x <5vnetwork@proton.me>
2026-05-21 18:31:07 +0800

    ui

--- lib/app/home.dart
+++ lib/app/home.dart
@@ -164,7 +164,7 @@ class _VpnHomePageState extends State<VpnHomePage> {
         backgroundColor: Colors.transparent,
         elevation: 0,
         leading: Platform.isMacOS ? null : settingButton,
-        leadingWidth: desktopPlatform ? 148 : 152,
+        leadingWidth: desktopPlatform ? 148 : 160,
         title: desktopPlatform
             ? ConstrainedBox(
                 constraints: BoxConstraints(maxHeight: 24),
